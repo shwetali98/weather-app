d0ea82a4b4bf2b85160c4e672fce1f11e17efc08
shwetali rane <shwetali98@gmail.com>
2025-03-23 14:41:28 -0700

    Working weather app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>🌤️ Weather Forecast App</h1>
+      <p>This is a basic React app structure.</p>
+    </div>
+  );
+}
+
+export default App;
